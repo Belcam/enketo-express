70bd5ff41e7df8343a45a61bab78aac03df8ac8f
tom <thomas.demaet@uclouvain.be>
2016-02-08 16:14:29 +0100

    mod get param to be more general in params possibilities and more precise on exec

--- public/js/get_param.js
+++ public/js/get_param.js
@@ -1,10 +1,46 @@
 /*
-**	Recupere uid et pid en param GET afin de pré remplire les inputs	
+**	Recupere tous les params GET afin de pré remplire les inputs	
 **
 **/
 
-setTimeout(function(){
-	var param = 'uid';
+// from http://gomakethings.com/a-native-javascript-equivalent-of-jquerys-ready-method/
+var ready = function ( fn ) {
+
+    // Sanity check
+    if ( typeof fn !== 'function' ) return;
+
+    // If document is already loaded, run method
+    if ( document.readyState === 'complete'  ) {
+        return fn();
+    }
+
+    // Otherwise, wait until document is loaded
+    // The document has finished loading and the document has been parsed but sub-resources such as images, stylesheets and frames are still loading. The state indicates that the DOMContentLoaded event has been fired.
+    // document.addEventListener( 'interactive', fn, false );
+
+    // Alternative: The document and all sub-resources have finished loading. The state indicates that the load event has been fired.
+    //document.addEventListener( 'complete', fn, false );
+    document.addEventListener( 'DOMContentLoaded', fn, false );
+
+};
+
+ready( function() {
+
+	var params = window.location.search.substring(1).split('&');
+        if(params.length <= 0)
+          return;
+	for(var i = 0; i < params.length; ++i) {
+		var param = params[i].split('=');
+		var key = param[0];
+		var val = decodeURIComponent(param[1]);
+		var elem = document.getElementsByName('/'+key.replace(/_/g, '/'));
+		if(elem && elem.length) {
+			elem[0].value = val;
+		}
+	}
+});
+//}, 500);
+	/*var param = 'uid';
 	
 	var uid = decodeURIComponent((new RegExp('[?|&]' + param + '=' + 
                 '([^&;]+?)(&|#|;|$)').exec(location.search)||[,""])[1].replace(/\+/g, 
@@ -25,6 +61,5 @@ setTimeout(function(){
 	{
 	    var pid_elem = document.getElementsByName('/newplot/plot/pid');
 		pid_elem[0].value=pid;
-	}
-		
-}, 500);
+	}*/
+//}, 500);
